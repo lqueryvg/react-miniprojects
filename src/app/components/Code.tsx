97966ba713341b81bf17ced92615c9b59dad9bb0
John Buxton <lqueryvg@gmail.com>
2025-01-29 15:00:52 +0000

    removing old mui based custom components

--- src/app/components/Code.tsx
+++ src/app/components/Code.tsx
@@ -3,13 +3,8 @@ export const Code = (props: {
   inline?: boolean;
 }) => {
   return props.inline ? (
-    // <code className="pl-1 pr-1 text-destructive">{props.children}</code>
     <code>{props.children}</code>
   ) : (
-    // <pre className="overflow-x-auto rounded-lg border-l-2 pl-6 leading-10 text-destructive">
-    //   {props.children}
-    // </pre>
-    // <pre className="prose">{props.children}</pre>
     <pre>{props.children}</pre>
   );
 };
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,3 @@
 export { Button } from "./Button";
 export { DemoPage } from "./DemoPage";
 export { URLTextLink } from "./URLTextLink";
-export { Menu } from "./Menu";
