871c8d6be215be7e1c2f5b7648b699f9e03d8b68
John Buxton <lqueryvg@gmail.com>
2025-02-11 19:44:47 +0000

    simplify ThemeSwitcher and default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import React from "react";
 import type { Viewport, Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 const inter = Inter({ subsets: ["latin"] });
 import "~/styles/globals.css";
 import { getPageMatterIndex } from "~/lib/pageMatter/getIndex";
-import React from "react";
 import { PageMatterProvider } from "~/lib/pageMatter/context";
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <div>
             <PageMatterProvider pages={pages}>
               <div>{children}</div>
