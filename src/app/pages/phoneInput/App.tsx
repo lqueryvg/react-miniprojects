4435c504a785149fa726a4690edc9523472029e6
John Buxton <lqueryvg@gmail.com>
2025-02-11 13:23:16 +0000

    fixed small glitch in phone input example

--- src/app/pages/phoneInput/App.tsx
+++ src/app/pages/phoneInput/App.tsx
@@ -2,44 +2,29 @@
 
 import { useState, useRef, useEffect } from "react";
 import { Input } from "@shadui/input";
-
-const formatPhoneNumber = (value: string) => {
-  let formattedText = value.replace(/[^\d]/g, ""); // remove all non-digits
-  if (formattedText.length > "123".length) {
-    formattedText = formattedText.replace(/(\d{3})/, "($1) "); // format "123" as "(123)""
-    if (formattedText.length > "(123) 456".length) {
-      formattedText = formattedText.replace(/\((\d{3})\) (\d{3})/, "($1) $2-"); // format "(123) 4567" as "(123) 456-7"
-    }
-  }
-  return formattedText;
-};
-
-interface Selection {
-  start: number;
-  end: number;
-}
+import { getNewValueAndCursorPosition } from "./helpers";
 
 export const App = () => {
   const [textValue, setTextValue] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
-  const [selection, setSelection] = useState<Selection>({ start: 0, end: 0 });
+  const [cursorPosition, setCursorPosition] = useState(0);
 
   useEffect(() => {
     // restore the position of the cursor
     if (inputRef.current) {
-      inputRef.current.setSelectionRange(selection.start, selection.end);
+      inputRef.current.setSelectionRange(cursorPosition, cursorPosition);
     }
-  }, [textValue, selection]);
+  }, [textValue, cursorPosition]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
-    const charsAdded = formattedPhoneNumber.length - textValue.length;
+    const unformattedPhoneNumber = e.target.value;
+    const oldCursorPosition = e.target.selectionStart!;
+
+    const { formattedPhoneNumber, newCursorPosition } =
+      getNewValueAndCursorPosition(unformattedPhoneNumber, oldCursorPosition);
+
     setTextValue(formattedPhoneNumber);
-    const selectionOffset = Math.abs(charsAdded) === 1 ? 0 : charsAdded;
-    setSelection({
-      start: e.target.selectionStart! + selectionOffset,
-      end: e.target.selectionEnd! + selectionOffset,
-    });
+    setCursorPosition(newCursorPosition);
   };
 
   return (
---
+++ src/app/pages/phoneInput/helpers.tsx
@@ -0,0 +1,63 @@
+const getDigitsOnly = (s: string) => {
+  return s.replace(/[^\d]/g, ""); // remove all non-digits
+};
+
+const formatPhoneNumber = (value: string) => {
+  let formattedText = getDigitsOnly(value);
+  if (formattedText.length > "123".length) {
+    formattedText = formattedText.replace(/(\d{3})/, "($1) "); // format "123" as "(123)""
+    if (formattedText.length > "(123) 456".length) {
+      formattedText = formattedText.replace(/\((\d{3})\) (\d{3})/, "($1) $2-"); // format "(123) 4567" as "(123) 456-7"
+    }
+  }
+  return formattedText;
+};
+
+const isDigit = (c: string) => {
+  return c >= "0" && c <= "9";
+};
+
+const countDigitsBeforeCursor = (text: string, cursorPosition: number) => {
+  let i = 0;
+  let digitCount = 0;
+  while (i < cursorPosition && i < text.length) {
+    if (isDigit(text.charAt(i))) {
+      digitCount++;
+    }
+    i++;
+  }
+  return digitCount;
+};
+
+const getCursorPositionAfterNDigits = (
+  text: string,
+  numDigitsBeforeCursor: number,
+) => {
+  let i = 0;
+  let digitCount = 0;
+  while (i < text.length) {
+    if (isDigit(text.charAt(i))) {
+      digitCount++;
+      if (digitCount === numDigitsBeforeCursor) return i + 1;
+    }
+    i++;
+  }
+  return 0;
+};
+
+export const getNewValueAndCursorPosition = (
+  unformattedPhoneNumber: string,
+  oldCursorPosition: number,
+) => {
+  const numDigitsBeforeCursor = countDigitsBeforeCursor(
+    unformattedPhoneNumber,
+    oldCursorPosition,
+  );
+
+  const formattedPhoneNumber = formatPhoneNumber(unformattedPhoneNumber);
+  const newCursorPosition = getCursorPositionAfterNDigits(
+    formattedPhoneNumber,
+    numDigitsBeforeCursor,
+  );
+  return { formattedPhoneNumber, newCursorPosition };
+};
